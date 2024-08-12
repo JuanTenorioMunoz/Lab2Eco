0cab1cc4e1b0ebdcb4c144ae8a9f0889b4b887f9
unknown <tenorio2003@gmail.com>
2024-08-11 20:04:06 -0500

    orgFind

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 document.getElementById("fetch-button").addEventListener("click", fetchPosts);
+const container = document.getElementById("data-container");
 
 async function fetchPosts() {
   renderLoadingState();
@@ -33,47 +34,43 @@ const fetchUsers = async () => {
 }
 
 function renderErrorState() {
-  const container = document.getElementById("data-container");
   container.innerHTML = ""; 
   container.innerHTML = "<p>Failed to load data</p>";
   console.log("Failed to load data");
 }
 
 function renderLoadingState() {
-  const container = document.getElementById("data-container");
   container.innerHTML = ""; 
   container.innerHTML = "<p>Loading...</p>";
   console.log("Loading...");
 }
 
 function renderData(data, users) {
-  const container = document.getElementById("data-container");
   container.innerHTML = ""; 
 
   if (data.length > 0) {
     data.forEach((item) => {
       const postContainer = document.createElement("div");
-      const user = document.createElement("h3")
-      const title = document.createElement("h1")
-      const body = document.createElement("p")
+      const user = document.createElement("h3");
+      const title = document.createElement("h1");
+      const body = document.createElement("p");
 
-      const foundUser = users.find((u) => u.id === users.userId)
-      console.log(foundUser)
+      const foundUser = findUser(users, item);
 
       postContainer.className = "post-container";
       title.innerHTML = item.title;
       body.innerHTML = item.body;
-      user.innerHTML = foundUser ? foundUser.name;
+      user.innerHTML = foundUser ? foundUser.name : "Unknown User";
       
       postContainer.appendChild(user);
       postContainer.appendChild(title);
       postContainer.appendChild(body);
-      container.appendChild(postContainer);  
+      container.appendChild(postContainer);
     });
   }
 }
 
 const findUser = (userArray, userId) => {
-  const found =  userArray.find((id) => id === userId)
+  const found =  userArray.find((id) => id.id === userId.id)
   return found
 }
